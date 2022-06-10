a687a5815fd3036e4d6dbd722f71badf7f6d516e
osamahCodes <osamahalghonaim@gmail.com>
2022-06-10 17:49:03 +0300

    Add the computerplay function

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function computerplay(){
+    let randomNumber= Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    if(randomNumber==1){
+        return 'Rock';
+    }else if (randomNumber==2) {
+        return 'Paper';
+    }else{
+        return 'Scissors';
+    }
+
+}
