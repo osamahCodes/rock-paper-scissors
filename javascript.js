d69bc50fb259b853802f4e52bd1758ab3c879cfd
osamahCodes <osamahalghonaim@gmail.com>
2022-06-10 18:11:57 +0300

    Add the playRound function

--- javascript.js
+++ javascript.js
@@ -1,11 +1,43 @@
-function computerplay(){
+function computerPlay(){
     let randomNumber= Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     if(randomNumber==1){
-        return 'Rock';
+        return 'rock';
     }else if (randomNumber==2) {
-        return 'Paper';
+        return 'paper';
     }else{
-        return 'Scissors';
+        return 'scissors';
     }
 
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection===computerSelection) {
+        return `Tie Game! both players played ${playerSelection}`;
+    }else if (playerSelection==='rock') {
+        if(computerSelection==='paper'){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+        }else{
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+        }
+    }
+    else if (playerSelection==='paper') {
+        if(computerSelection==='scissors'){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+        }else{
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+        }
+    }
+    else{
+        if(computerSelection==='rock'){
+            return `You Lose! ${computerSelection} beats ${playerSelection}`
+        }else{
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+        }
+        
+    }
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
