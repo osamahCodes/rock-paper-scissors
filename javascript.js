0a275d0a3f707f0ec18223cc77b4af000e54486e
osamahCodes <osamahalghonaim@gmail.com>
2022-06-13 17:21:19 +0300

    Complete all required work on ui and logic

--- javascript.js
+++ javascript.js
@@ -10,41 +10,45 @@ function computerPlay(){
 
 }
 
-function playRound(computerSelection) {
-    let playerSelection = this.classList.value;
-    round.textContent = (parseInt(round.textContent)+1).toString();
+function playRound(computerSelection, playerSelection) {
+    // console.log(this);
+    // let playerSelection = this.classList.value;
+    // console.log(playerSelection);
     if (playerSelection===computerSelection) {
+        // computerScore.textContent = (parseInt(computerScore.textContent, 10)+1).toString();
+        // playerScore.textContent = (parseInt(playerScore.textContent, 10)+1).toString();
+        round.textContent = (parseInt(round.textContent, 10)-1).toString();
         return;
         // return `Tie Game! both players played ${playerSelection}`;
     }else if (playerSelection==='rock') {
         if(computerSelection==='paper'){
-            computerScore.textContent = (parseInt(computerScore.textContent)+1).toString();
+            computerScore.textContent = (parseInt(computerScore.textContent, 10)+1).toString();
             return;
             // return `You Lose! ${computerSelection} beats ${playerSelection}`
         }else{
-            playerScore.textContent = (parseInt(playerScore.textContent)+1).toString();
+            playerScore.textContent = (parseInt(playerScore.textContent, 10)+1).toString();
             return;
             // return `You Win! ${playerSelection} beats ${computerSelection}`
         }
     }
     else if (playerSelection==='paper') {
         if(computerSelection==='scissors'){
-            computerScore.textContent = (parseInt(computerScore.textContent)+1).toString();
+            computerScore.textContent = (parseInt(computerScore.textContent, 10)+1).toString();
             return;
             // return `You Lose! ${computerSelection} beats ${playerSelection}`
         }else{
-            playerScore.textContent = (parseInt(playerScore.textContent)+1).toString();
+            playerScore.textContent = (parseInt(playerScore.textContent, 10)+1).toString();
             return;
             // return `You Win! ${playerSelection} beats ${computerSelection}`
         }
     }
     else{
         if(computerSelection==='rock'){
-            computerScore.textContent = (parseInt(computerScore.textContent)+1).toString();
+            computerScore.textContent = (parseInt(computerScore.textContent, 10)+1).toString();
             return;
             // return `You Lose! ${computerSelection} beats ${playerSelection}`
         }else{
-            playerScore.textContent = (parseInt(playerScore.textContent)+1).toString();
+            playerScore.textContent = (parseInt(playerScore.textContent, 10)+1).toString();
             return;
             // return `You Win! ${playerSelection} beats ${computerSelection}`
         }
@@ -53,18 +57,41 @@ function playRound(computerSelection) {
 }
 
 function game(){
-    const buttons = document.querySelectorAll('button');
-    const playerScore = document.querySelector('player');
-    const computerScore = document.querySelector('computer');
-    const round = document.querySelector('round');
-    buttons.forEach(button => button.addEventListener('click', playRound(computerPlay())))
+    const buttons = document.querySelector('.buttons');
+
+    function listens(e){
+        round.textContent = (parseInt(round.textContent, 10)+1).toString();
+        
+        const isButton = e.target.nodeName === 'BUTTON';
+        // console.log(isButton);
+        if(!isButton){
+            return;
+        }else{
+            playRound(computerPlay(), e.target.classList.value);
+        }
+        if((parseInt(playerScore.textContent,10))===5){
+            buttons.removeEventListener('click', listens);
+            footer.textContent = 'You won!!! restart to try again';
+
+            
+        }else if((parseInt(computerScore.textContent,10))===5){
+            buttons.removeEventListener('click', listens);    
+            footer.textContent = 'You lost :( restart to try again';
+  
+        }
+    }
+
+    buttons.addEventListener('click', listens)
+    
+
     // for (let i = 0; i < 5; i++) {
     //     // let playerSelection = prompt("rock, paper, or scissors?");
     //     let computerSelection = computerPlay();
     //     console.log(playRound(playerSelection, computerSelection));
     // }
 }
-
-
-
+let playerScore = document.querySelector('.player');
+let computerScore = document.querySelector('.computer');
+let round = document.querySelector('.round');
+let footer = document.querySelector('.footer');
 game()
